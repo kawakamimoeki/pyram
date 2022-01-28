28b62866a3f43429ba041c4c3429780cd971f631
moekidev <me@moeki.dev>
2022-01-28 12:34:59 +0900

    家計簿があったらリダイレクト

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,7 @@ class HomeController < ApplicationController
   layout "landing_page"
 
   def index
+    redirect_to book_path(current_book) if current_book
     @expense = Expense.new
     @sample_book = Book.find_by(name: "Sample Book")
   end
