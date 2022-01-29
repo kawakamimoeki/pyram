1e36da2ca71d963193efc92cc9f5d903345ec269
moekidev <me@moeki.dev>
2022-01-29 09:23:42 +0900

    colorfulize expense form

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -4,9 +4,11 @@ import { LineController } from '../controllers/line_controller'
 import { DoughnutController } from '../controllers/doughnut_controller'
 import { CardsController } from '../controllers/cards_controller'
 import { AddtionController } from '../controllers/addition_controller'
+import { ColorizeController } from '../controllers/colorize_controller'
 
 const application = Application.start()
 application.register('line', LineController)
 application.register('doughnut', DoughnutController)
 application.register('cards', CardsController)
 application.register('addition', AddtionController)
+application.register('colorize', ColorizeController)
