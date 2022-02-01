73f9cad44879123205580d7883e8de3221c5dbdc
moekidev <me@moeki.dev>
2022-02-01 15:51:57 +0900

    remove unnecesary files

--- app/models/paymnet.rb
+++
@@ -1,2 +0,0 @@
-class Paymnet < ApplicationRecord
-end
--- test/models/paymnet_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class PaymnetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
