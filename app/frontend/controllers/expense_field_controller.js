db641687f18e8326ef42a597c2fc67026f28fc86
moekidev <me@moeki.dev>
2022-02-10 17:41:10 +0900

    enable bidirectional automatic input

--- app/frontend/controllers/expense_field_controller.js
+++ app/frontend/controllers/expense_field_controller.js
@@ -3,7 +3,11 @@ import { Controller } from '@hotwired/stimulus'
 export class ExpenseFieldController extends Controller {
   static targets = ['all', 'required', 'affluent']
 
-  call () {
-    this.affluentTarget.value = this.allTarget.value - this.requiredTarget.value
+  call (event) {
+    if (event.target === this.affluentTarget) {
+      this.requiredTarget.value = this.allTarget.value - this.affluentTarget.value
+    } else if (event.target === this.requiredTarget) {
+      this.affluentTarget.value = this.allTarget.value - this.requiredTarget.value
+    }
   }
 }
